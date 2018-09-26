eb4812c350f1ce7496ceb88b5dcbdfab04a86b18
Victor Heine Holten <vict1856@edu.eal.dk>
2018-09-26 09:26:45 +0200

    added line with text

--- git1/Program.cs
+++ git1/Program.cs
@@ -10,6 +10,9 @@ namespace git1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("hello git1");
+            Console.WriteLine("ny ændring");
+            Console.ReadLine();
         }
     }
 }
